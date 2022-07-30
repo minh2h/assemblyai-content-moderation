5ef80b44583929930f371beaf23e8940fc5bce1d
minh2h <48518959+minh2h@users.noreply.github.com>
2022-07-30 19:19:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ if submit_button:
         )
 
 with st.sidebar.expander('Example URL'):
-	st.code('https://www.youtube.com/watch?v=_I2bxeQEa6Y')
+	st.code('https://www.youtube.com/watch?v=_I2bxeQEa6Y');
     st.code('https://www.youtube.com/watch?v=S0dSm_ClcSw')
